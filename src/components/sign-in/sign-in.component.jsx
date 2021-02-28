0a58c565468afe10399c66bab81f238416e75064
Buddy Harlow <catlinharlow1758@gmail.com>
2021-02-28 16:55:01 -0500

    Building out remainder of form

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './sign-in.styles.scss'
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+
+class SignIn extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+
+        }
+    }
+
+    handleChange = event => {
+       const { value, name } = event.target;
+            this.setState({ [name]: value })
+            
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({email: '', password: ''})
+    }
+
+    render() {
+        return(
+            <div className='sign-in'>
+                <h2 className='title'>I already have an account</h2>
+                <span>Sign in with you email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput name="email" type="email" value={this.state.email} label="email" handleChange={this.handleChange} required />
+                    <FormInput name="password" type="password" value={this.state.password} label="password" handleChange={this.handleChange} required />
+                    
+                    <CustomButton type="submit">Sign in</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
